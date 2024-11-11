72c1c53a0e6f6ef2b96ebbcf0ca0cc3e224bc719
pr4nv-1000 <pranavbrajyaguru@gmail.com>
2024-11-11 14:40:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,9 @@ app.use(express.json());
 app.use(cors());
 
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 30000
+}).then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
 
